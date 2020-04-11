06f62e7da67f4b151c999e68e5badc8895fb9490
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-04-10 18:14:50 -0700

    started api routes & model for sign in

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var bcrypt = require("bcrypt");
 
 module.exports = function(app) {
   // Get all examples
@@ -24,5 +25,46 @@ module.exports = function(app) {
     ) {
       res.json(dbRestaurant);
     });
+  });
+
+  app.post("/api/user", function(req, res) {
+
+    db.Restaurant.create(req.body).then(function() {
+      return res.send(true);
+    }).catch(function(error) {
+      return res.status(500).send("Something went wrong.");
+    });
+  });
+
+  app.post("/api/signin", function(req, res) {
+    const { email, password } = req.body;
+
+    db.Restaurant.findOne({
+      where: {
+        email,
+      },
+    }).then(function(user) {
+      if (!user) {
+        return res.status(406).send("User not found.");
+      }
+      
+      bcrypt.compare(myPlaintextPassword, has, function(err,result) {
+if (err) {
+  console.log(err);
+
+  return res.status(500).send("Service unavailable");  
+}
+
+if (!result) {
+  return res.status(401).send("Unauthorized");
+}
+res.send({
+  jwt: "JWT GOES HERE",
+});
+
+      });
+
+    });
+    
   });
 };
