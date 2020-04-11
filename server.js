da454aef1259a9bb26bae26de8cd2d79965d2e81
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-04-10 22:50:46 -0700

    added a few more lines of code for sign in

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+// var exphbs = require("express-handlebars");
+var helmet = require("helmet");
+var bearerToken = require("express-bearer-token");
 
 var db = require("./models");
 
 var app = express();
+
+app.use(bearerToken());
+
+app.use(helmet());
+
 var PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -12,15 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    // eslint-disable-next-line prettier/prettier
-    defaultLayout: "main",
-  })
-);
-app.set("view engine", "handlebars");
+// // Handlebars
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     // eslint-disable-next-line prettier/prettier
+//     defaultLayout: "main",
+//   })
+// );
+// app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
